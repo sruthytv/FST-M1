d66c35843cdd5df65dde3bc9dcb1b245cb7b3bb7
sruthytv <sruthytv666@gmail.com>
2022-05-06 20:52:18 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity11_2.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity11_2 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+
+        //Open browser
+        driver.get("https://www.training-support.net/selenium/javascript-alerts");
+
+        String title = driver.getTitle();
+        //Print the title of the page
+        System.out.println("Page title is: " + title);
+
+        //Click the button to open a simple alert
+        driver.findElement(By.cssSelector("button#confirm")).click();
+
+        //Switch to alert window
+        Alert confirmAlert = driver.switchTo().alert();
+
+        //Get text in the alert box and print it
+        String alertText = confirmAlert.getText();
+        System.out.println("Alert text is: " + alertText);
+
+        //Close the alert with OK
+        confirmAlert.accept();
+
+        //Close the browser with Cancel
+        //confirmAlert.dismiss();
+
+        //Close the Browser
+        driver.close();
+    }
+}
---
+++ Selenium/Activities/Activity5_3.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity5_3 {
+
+    public static void main(String[] args) {
+
+        // Create a new instance of the Firefox driver
+        WebDriver driver = new FirefoxDriver();
+
+        //Open the browser
+        driver.get("https://training-support.net/selenium/dynamic-controls");
+
+        String title = driver.getTitle();
+        //Print the title of the page
+        System.out.println("Page title is: " + title);
+
+        //Find the checkbox
+        WebElement textInput = driver.findElement(By.xpath("//input[@type='text']"));
+
+        //Print status
+        System.out.println("The checkbox is selected: " + textInput.isEnabled());
+
+        driver.findElement(By.id("toggleInput")).click();
+
+        //Print status
+        System.out.println("The checkbox is selected: " + textInput.isEnabled());
+
+        //Close the browser
+        driver.close();
+
+    }
+
+}
